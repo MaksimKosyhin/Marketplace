66fe6dac19bf08dc6f888796e9a969c53f975d0b
Maksim Kosyhin <maksimkosihyn@gmail.comgit config --global user.name Maksim>
2022-08-30 14:19:10 +0300

    Fixed project
    Added driver to properties, rewrote config with use of '@value',
    formatted sql

--- Marketplace/src/main/java/com/example/demo/config/DataSourceConfig.java
+++ Marketplace/src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -3,25 +3,37 @@ package com.example.demo.config;
 import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @Configuration
 public class DataSourceConfig {
-	@Autowired
-	private Environment env;
+	private final String driverClassName;
+	private final String url;
+	private final String username;
+	private final String password;
 	
+	public DataSourceConfig(@Value("${spring.datasource.driverClassName}") String driverClassName, 
+							@Value("${spring.datasource.url}") String url,
+							@Value("${spring.datasource.username}") String username,
+							@Value("${spring.datasource.password}") String password) {
+		
+		this.driverClassName = driverClassName;
+		this.url = url;
+		this.username = username;
+		this.password = password;
+	}
+
 	@Bean
 	public DataSource dataSource() {
 		return DataSourceBuilder
 				.create()
-				.driverClassName("org.postgresql.Driver")
-				.url(env.getProperty("spring.datasource.url"))
-				.username(env.getProperty("spring.datasource.username"))
-				.password(env.getProperty("spring.datasource.password"))
+				.driverClassName(driverClassName)
+				.url(url)
+				.username(username)
+				.password(password)
 				.build();
 	}
 	

c1ce564e85bf01bc10454e782f47969fbb95f3f1
Maksim Kosyhin <maksimkosihyn@gmail.comgit config --global user.name Maksim>
2022-08-30 15:51:42 +0300

    refactored config

--- Marketplace/src/main/java/com/example/demo/config/DataSourceConfig.java
+++ Marketplace/src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -10,24 +10,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataSourceConfig {
-	private final String driverClassName;
-	private final String url;
-	private final String username;
-	private final String password;
-	
-	public DataSourceConfig(@Value("${spring.datasource.driverClassName}") String driverClassName, 
-							@Value("${spring.datasource.url}") String url,
-							@Value("${spring.datasource.username}") String username,
-							@Value("${spring.datasource.password}") String password) {
-		
-		this.driverClassName = driverClassName;
-		this.url = url;
-		this.username = username;
-		this.password = password;
-	}
 
 	@Bean
-	public DataSource dataSource() {
+	public DataSource dataSource(@Value("${spring.datasource.driverClassName}") String driverClassName, 
+								 @Value("${spring.datasource.url}") String url,
+								 @Value("${spring.datasource.username}") String username,
+								 @Value("${spring.datasource.password}") String password) {
 		return DataSourceBuilder
 				.create()
 				.driverClassName(driverClassName)
@@ -38,10 +26,10 @@ public class DataSourceConfig {
 	}
 	
 	@Bean
-	public Flyway flyway() {
+	public Flyway flyway(DataSource dataSource) {
 		Flyway flyway = Flyway
 				.configure()
-				.dataSource(dataSource())
+				.dataSource(dataSource)
 				.load();
 		
 		flyway.migrate();

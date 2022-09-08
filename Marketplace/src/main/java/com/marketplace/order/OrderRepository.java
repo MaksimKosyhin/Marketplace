8468df6ab762da58ef49a324fa735a21dc4e301c
Maksim Kosyhin <maksimkosihyn@gmail.comgit config --global user.name Maksim>
2022-09-08 23:36:15 +0300

    implemented order repository

---
+++ Marketplace/src/main/java/com/marketplace/order/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.marketplace.order;
+
+import java.util.List;
+
+public interface OrderRepository {
+    public long addOrder(long userId);
+    public List<OrderedProduct> getOrder(long userId, long orderId);
+    public List<OrderedProduct> getAllOrders(long userId);
+    public boolean addProductToOrder(OrderQuery orderQuery);
+    public boolean changeProductAmount(OrderQuery orderQuery);
+    public long getCurrentOrderId(String username);
+    public long getUserId(String username);
+}
